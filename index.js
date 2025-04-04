8e44b9a008ef3a219e69ad8a8db0b84bdd4afa4f
unknown <srijancodex@gmail.com>
2025-04-04 13:48:55 +0530

    earth

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import * as THREE from "three";
 import { OrbitControls } from "jsm/controls/OrbitControls.js";
 
+
 const w = window.innerWidth;
 const h = window.innerHeight;
 const renderer = new THREE.WebGLRenderer({ antialias: true })
@@ -15,22 +16,50 @@ const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
 camera.position.z = 5;
 
-
-const controls = new OrbitControls(camera, renderer.domElement)
+const earthGroup = new THREE.Group()
+earthGroup.rotation.z = -23.4 * Math.PI / 180;
+scene.add(earthGroup)
+new OrbitControls(camera, renderer.domElement)
 const loader = new THREE.TextureLoader()
-const geometry = new THREE.IcosahedronGeometry(1, 8);
-const material = new THREE.MeshStandardMaterial({
+const detail = 12;
+const geometry = new THREE.IcosahedronGeometry(1, detail);
+
+
+const earthmaterial = new THREE.MeshStandardMaterial({
     map: loader.load("./static/assets/earthmap1k.jpg")
 })
-const earthMesh = new THREE.Mesh(geometry, material);
-scene.add(earthMesh)
+const earthMesh = new THREE.Mesh(geometry, earthmaterial);
+earthGroup.add(earthMesh)
 
-const hemilight = new THREE.HemisphereLight(0xffffff)
-scene.add(hemilight)
+const lightMaterial = new THREE.MeshStandardMaterial({
+    map: loader.load("./static/assets/earthlights1k.jpg"),
+    blending:THREE.AdditiveBlending
+})
+const lightMesh = new THREE.Mesh(geometry, lightMaterial)
+earthGroup.add(lightMesh)
+
+const cloudMaterial = new THREE.MeshStandardMaterial({
+    map:loader.load("./static/assets/earthcloudmaptrans.jpg"),
+    transparent:true,
+    opacity:0.5,
+    blending :THREE.AdditiveBlending
+})
+const cloudmesh = new THREE.Mesh(geometry , cloudMaterial);
+cloudmesh.scale.setScalar(1.01)
+earthGroup.add(cloudmesh);
+
+// const hemilight = new THREE.HemisphereLight(0xffffff,0x444444)
+// scene.add(hemilight)
+
+const sunLight = new THREE.DirectionalLight(0xffffff)
+sunLight.position.set(-2, 0.5, 1.5);
+scene.add(sunLight)
 
 function animate(t = 0) {
     requestAnimationFrame(animate)
-    earthMesh.rotation.y += 0.005;
+    earthMesh.rotation.y += 0.001;
+    lightMesh.rotation.y += 0.001;
+    cloudmesh.rotation.y += 0.001;
     renderer.render(scene, camera)
 }
 animate();
